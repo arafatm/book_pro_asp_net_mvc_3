88a96b61db5002ca17300e75123133a68671ecd7
Arafat Mohamed <github@arafatm.com>
2013-02-04 14:23:11 -0600

    5.2. Consuming a Property

--- scratch/ch5_essential_features/Program.cs
+++ scratch/ch5_essential_features/Program.cs
@@ -20,6 +20,16 @@ namespace ch5_essential_features
   {
     static void Main(string[] args)
     {
+
+      // create a new Product object
+      Product myProduct = new Product();
+
+      // set the property value
+      myProduct.Name = "Kayak";
+
+      // get the property
+      string productName = myProduct.Name;
+      Console.WriteLine("Product name: {0}", productName);
     }
   }
 }
