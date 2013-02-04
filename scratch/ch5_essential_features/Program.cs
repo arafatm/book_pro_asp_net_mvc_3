028e9d89210a46cdc3780fadd15a1b6dbb9d3134
Arafat Mohamed <github@arafatm.com>
2013-02-04 14:16:57 -0600

    Eg 5.1.1 Defining a Property

---
+++ scratch/ch5_essential_features/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ch5_essential_features
+{
+  public class Product
+  {
+    private string name;
+
+    public string Name
+    {
+      get { return name; }
+      set { name = value; }
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}
